7d01533ba0bd4b5718270e71f09bf4cd100c4ae0
Your Name <you@example.com>
2018-08-02 20:00:09 +1000

    until site 44 in android textbook

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,35 +1,82 @@
 package com.example.geoquiz;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton; //"m" prefix on the member (instance) variable names is an Android naming convention
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+
+    //Array for Questions
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //get refernece for the TextView and set its text to the question at the current index
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        //encapsulating with update Question()
+       /* int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);*/
+
         //Retrieve inflated objects and assign them to member variables:
         mTrueButton = (Button) findViewById(R.id.true_button);
         // setting listeners as an anonymous inner class (new View.OnClickListener()...)
-        mTrueButton.setOnClickListener(new View.OnClickListener(){
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+
         mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener(){
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+
+                //encapsulating with update Question()
+                /*int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);*/
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
     }
 
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
 }
